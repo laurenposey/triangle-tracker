11b2e62e2c7aad6abdf0aa9e1fc8e99163ed84b4
Carl Egbert and Lauren Posey <carl@>
2016-06-02 14:00:03 -0700

    add scripts for submitting and evaluating triangle.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $("form#triangleInput").submit(function(event){
+    event.preventDefault();
+    var side1 = parseInt($("#side1").val());
+    var side2 = parseInt($("#side2").val());
+    var side3 = parseInt($("#side3").val());
+
+    alert(checkSides(side1,side2,side3));
+
+
+  });
+});
+
+var checkSides = function(s1,s2,s3) {
+
+  if ((s1===s2)&&(s1===s3)) {
+    return "Your triangle is an equilateral triangle.";
+  } else if (s1+s2<=s3||s2+s3<=s1||s1+s3<=s2) {
+    return "That's not a triangle.";
+  } else if ((s1!=s3 && s1!=s2) && s2!=s3) {
+    return "Your triangle is a scalene triangle.";
+  } else if (s1===s2||s1===s3||s2===s3) {
+    return "Your triangle is an isosceles triangle.";
+  }
+}
